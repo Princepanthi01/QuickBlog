1fe6f1b3642a2cd888b4d41c5030b261f05446f0
Prince Panthi <panthiprins@gmail.com>
2025-07-26 00:40:20 +0530

    now completed

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 
 import BlogTableItem from '../../components/admin/BlogTableItem';
 import { useAppContext } from '../../context/AppContext';
